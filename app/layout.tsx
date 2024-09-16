210c8f1c2d7df1f4cb771e6e4b2d526b5cfe0486
Aditya Punmiya <98465454+Aditya-0011@users.noreply.github.com>
2024-09-16 15:02:04 +0530

    added clarity

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,6 @@
 import type { Viewport } from "next";
+import Script from 'next/script'
+
 import { Inter } from "next/font/google";
 import "./globals.css";
 
@@ -23,6 +25,15 @@ export default function RootLayout({
       lang="en"
       className="scrollbar-thin overflow-y-scroll scrollbar-thumb-neutral-900 scrollbar-track-neutral-950"
     >
+      <Script id="clarity-script" strategy="afterInteractive">
+          {`
+            (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "${process.env.CLARITY_KEY as string}");
+          `}
+        </Script>
       <body className={`${inter.className} min-w-96 overflow-x-hidden`}>
         <Navbar />
         {children}

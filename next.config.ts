fa30cb48bfddec7793622e4c87b3b289847856d7
Aditya Punmiya <adityapunmiya@gmail.com>
2025-05-08 10:11:55 +0530

    refactor: remove unused headers configuration from next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,27 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: { remotePatterns: [{ hostname: "**" }] },
-  async headers() {
-    return [
-      {
-        source: "/(.*)",
-        headers: [
-          {
-            key: "X-Content-Type-Options",
-            value: "nosniff",
-          },
-          {
-            key: "X-Frame-Options",
-            value: "DENY",
-          },
-          {
-            key: "Referrer-Policy",
-            value: "strict-origin-when-cross-origin",
-          },
-        ],
-      },
-    ];
-  },
 };
 
 const withBundleAnalyzer = require("@next/bundle-analyzer")({

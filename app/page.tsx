c7d9ea1fa17a58127791de6ca8a564c3466b6cf8
Aditya Punmiya <98465454+Aditya-0011@users.noreply.github.com>
2025-06-28 19:48:29 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -9,11 +9,11 @@ export const revalidate = 600;
 
 export const metadata: Metadata = {
   title: "Home | Aditya Punmiya",
-  description: "View my bio, featured projects and technologies.",
+  description: "View my bio, work experience, projects and technologies.",
   metadataBase: new URL("https://adityapunmiya.com"),
   openGraph: {
     title: "Home | Aditya Punmiya",
-    description: "View my bio, featured projects and technologies.",
+    description: "View my bio, work experience, projects and technologies.",
     siteName: "Aditya Punmiya",
     type: "website",
     url: "https://adityapunmiya.com",
@@ -26,7 +26,7 @@ export const metadata: Metadata = {
   twitter: {
     card: "summary_large_image",
     title: "Home | Aditya Punmiya",
-    description: "View my bio, featured projects and technologies.",
+    description: "View my bio, work experience, projects and technologies.",
     images: [
       {
         url: "https://res.cloudinary.com/dijxynt89/image/upload/v1725052376/Aditya_os4fzb.jpg",

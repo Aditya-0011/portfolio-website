383f3a65b73cee6908c7f4e5e8a335f006e90c09
Aditya Punmiya <98465454+Aditya-0011@users.noreply.github.com>
2025-06-28 19:42:58 +0530

    Update ExperienceDisplay.tsx

--- components/home/experiences/ExperienceDisplay.tsx
+++ components/home/experiences/ExperienceDisplay.tsx
@@ -8,7 +8,7 @@ export default async function ExperienceDisplay() {
   const experiences: Experience[] = await getExperiences();
 
   return (
-    <div className="py-8 sm:py-16">
+    <div className="py-8 sm:py-16" id="experience">
       <div className="mx-auto max-w-7xl px-6 lg:px-8">
         <div className="max-w-2xl lg:mx-0">
           <h2 className="text-3xl font-bold tracking-tight text-white sm:text-4xl">

d0a98698ca57b186eff92d4d6ba796eea7cdf19b
Aditya Punmiya <adityapunmiya@gmail.com>
2025-07-27 13:17:31 +0530

    feat: implement cache control for resume.pdf in next.config.ts and remove vercel.json

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,19 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: { remotePatterns: [{ hostname: "**" }], minimumCacheTTL: 2678400 },
+  async headers() {
+    return [
+      {
+        source: "/resume.pdf",
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "no-cache, no-store, must-revalidate",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 const withBundleAnalyzer = require("@next/bundle-analyzer")({
